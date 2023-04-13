32ea16b49e1d775299453b69881895eca1a943c7
KimbohLovette <kimbohlovette@gmail.com>
2023-04-13 12:57:04 +0100

    Improved on limit and page types in paginated categories controller

--- src/controllers/category/getPaginatedCategories.ts
+++ src/controllers/category/getPaginatedCategories.ts
@@ -2,12 +2,13 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/categoryModel';
 
 export const getPaginatedCategories = async (req: Request, res: Response) => {
-	const { limit = 10, page = 1 } = req.query;
+	const limit = Number(req.query['limit']);
+	const page = Number(req.query['page']);
 	try {
 		const categories = (
 			await Category.find({})
-				.limit(+limit)
-				.skip((+limit - 1) * +limit)
+				.limit(limit)
+				.skip((page - 1) * limit)
 		).map((cat) => ({
 			id: cat._id,
 			name: cat.name,
