7aca0d443beaa910f77fe8470cac187243141393
KimbohLovette <kimbohlovette@gmail.com>
2023-04-26 01:57:03 +0100

    Removed the server module and transfered code to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
-import app from './server';
 import express from 'express';
 import askRoutes from './routes/askRoutes';
 import userRoutes from './routes/userRoutes';
 import categoryRoutes from './routes/categoryRoutes';
+import mongoose from 'mongoose';
+
+const app = express();
+
+mongoose
+	.connect(
+		'mongodb+srv://kimbohlovette:CqaluLRZNBZNKrmS@fitnesscoachdb.8a1ynfl.mongodb.net/who-get?retryWrites=true&w=majority'
+	)
+	.then(() => {
+		app.listen(5000, () => {
+			console.log('Listening on port 5000....');
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 
 app.use(express.static('./public'));
 app.use(express.json());
